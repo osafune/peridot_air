3ac5723ed86f088cd6fcf663e621c397500a62e3
Shun OSAFUNE <s.osafune@gmail.com>
2022-05-30 14:42:44 +0900

    release

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# peridot_air
- 
+PERIDOT-Air
+===========
+
+Overview
+--------
+<img src="https://raw.githubusercontent.com/osafune/peridot_air/master/img/air_thumb.jpg">
+
+PERIDOT-AIRは Intel社製 CycloneIV E FPGA(EP4CE6E22C8N)を搭載したプロトタイピング向けFPGAボードです。
+FPGAの他に256MbitのSDRAM、SD/MMCカードスロット、OSCおよびコンフィギュレーションROMをオンボードに搭載しており、インスタントオンで動作させることができます。
+搭載FPGAは約15万ゲート規模で、ソフトマクロCPUを使った組み込み用SoC開発やディジタル回路の実装に最適です。
+ボード形状はArduinoと同一になっており、Arduino用のホルダーや各種シールドを使うことができます。
+
+- 搭載FPGAデバイス
+	- CycloneIV Eファミリ EP4CE6E22C8N
+	(6kLE, 内蔵メモリ 270kbit, PLL 2個, 18x18乗算器 15個)
+- オンボード周辺機器
+	- SDRAM x16bit幅 256Mbit
+	- コンフィギュレーションROM EPCQ16互換 16Mbit
+	- MEMS OSC 50MHz
+	- SD/MMCカードスロット
+	- ユーザーLED 2個
+- ユーザーI/O数
+	- 28本（Arduinoタイプピンソケット）
+	I/O電圧は3.3V、5V信号入力は不可
+- 動作電圧
+	- オンボードmicro-Bコネクタからの4.5V～5.5V、またはArduinoピンソケットのVIN端子からの4.5V～9V
+	FPGA電源(1.2V、2.5V)およびI/O電源(3.3V)はオンボードで生成
+
+
+Board Pinout
+------------
+<img src="https://raw.githubusercontent.com/osafune/peridot_air/master/img/air_pinout.png">
+[Large size](https://raw.githubusercontent.com/osafune/peridot_air/master/img/air_pinout_large.png)
+
+
+JTAG Connector
+--------------
+同梱のJTAG-CONN変換基板を下記のように接続してUSB-Blasterダウンロードケーブルを使用してください。
+<img src="https://raw.githubusercontent.com/osafune/peridot_air/master/img/air_jtagconn.jpg" width="600" height="333">
+
+
+Resource
+--------
+- [回路図](https://raw.githubusercontent.com/osafune/peridot_air/master/pcb/peridot_air_pcb_schem_rev1.1r.pdf)
+
+
+Documents
+---------
+- [QuartusPrime LiteEdition ダウンロード](https://www.intel.com/content/www/us/en/software-kit/684216/intel-quartus-prime-lite-edition-design-software-version-21-1-for-windows.html)
+- [Intel CycloneIV E のドキュメント](https://www.intel.co.jp/content/www/jp/ja/products/details/fpga/cyclone/iv/docs.html?s=Newest)
+
+
+Contant Us
+----------
+- [GitHub - Shun OSAFUNE](https://github.com/osafune)
+- [Twitter - @s_osafune](https://twitter.com/s_osafune)
---
+++ fpga/air_blank_top/air_blank_top.v
@@ -0,0 +1,112 @@
+// ===================================================================
+// TITLE : PERIDOT-Air Blank top
+//
+//     DESIGN : s.osafune@j7system.jp (J-7SYSTEM WORKS LIMITED)
+//     DATE   : 2018/01/19 -> 2018/03/22
+//     UPDATE : 2020/04/21
+//
+// ===================================================================
+//
+// The MIT License (MIT)
+// Copyright (c) 2020 J-7SYSTEM WORKS LIMITED.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+`default_nettype none
+
+module air_blank_top(
+	// clk and system reset
+	input wire			CLOCK_50,
+	input wire			RESET_N,
+
+	// Interface: EPCS memory controller
+	output wire			EPCS_CSO_N,
+	output wire			EPCS_DCLK,
+	output wire			EPCS_ASDO,
+	input wire			EPCS_DATA0,
+
+	// Interface: SD-card controller
+	output wire			SD_DAT3,
+	output wire			SD_CLK,
+	output wire			SD_CMD,
+	input wire			SD_DAT0,
+
+	// Interface: SD-card Power enable
+	output wire			SD_PWR_N,
+
+	// Interface: SD-card Detect
+	input wire			SW_CD_N,
+
+	// Interface: SDRAM
+	output wire			SDRCLK_OUT,
+	output wire			SDR_CKE,
+	output wire			SDR_CS_N,
+	output wire			SDR_RAS_N,
+	output wire			SDR_CAS_N,
+	output wire			SDR_WE_N,
+	output wire [12:0]	SDR_A,
+	output wire [1:0]	SDR_BA,
+	inout wire  [15:0]	SDR_DQ,
+	output wire [1:0]	SDR_DQM,
+
+	// GPIO
+	inout wire  [27:0]	D,
+
+	// OnBoard LED
+	output wire [1:0]	USER_LED
+);
+
+
+/* ===== 外部変更可能パラメータ ========== */
+
+
+
+/* ----- 内部パラメータ ------------------ */
+
+
+/* ※以降のパラメータ宣言は禁止※ */
+
+/* ===== ノード宣言 ====================== */
+				/* 内部は全て正論理リセットとする。ここで定義していないノードの使用は禁止 */
+	wire			reset_sig = ~RESET_N;			// モジュール内部駆動非同期リセット 
+
+				/* 内部は全て正エッジ駆動とする。ここで定義していないクロックノードの使用は禁止 */
+	wire			clock_sig = CLOCK_50;			// モジュール内部駆動クロック 
+
+
+
+/* ※以降のwire、reg宣言は禁止※ */
+
+/* ===== テスト記述 ============== */
+
+
+
+/* ===== モジュール構造記述 ============== */
+
+	assign {EPCS_DCLK,EPCS_ASDO,EPCS_CSO_N} = 3'b001;		// コンフィグROMをユーザーモードで使わない場合 
+	assign {SD_CLK,SD_CMD,SD_DAT3,SD_PWR_N} = 4'bzzz1;		// SDカードを使わない場合 
+	assign {SDRCLK_OUT,SDR_CKE} = 2'b00;					// SDRAMを使わない場合 
+
+
+
+
+
+
+endmodule
